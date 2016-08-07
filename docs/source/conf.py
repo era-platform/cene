7603cf0bce367ed931c07459eb68e1d35a4e32bf
Ross Angle <rokitna@hotmail.com>
2016-08-07 02:47:06 -0700

    Wrote a basic Cene syntax highlighter for the Sphinx docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,10 @@ _copyright_dates = u'2016'
 
 import json
 import os
+import re
 from recommonmark.parser import CommonMarkParser
+from pygments.lexer import RegexLexer
+from pygments.token import Comment, Name, Keyword, Text
 
 _readthedocs = os.environ.get('READTHEDOCS', None) == 'True'
 
@@ -14,6 +17,45 @@ if not _readthedocs:
 
 with open('../../package.json') as package_json_file:
     _package_json = json.load(package_json_file)
+
+
+# A really basic Pygments lexer for Cene programs
+
+# This is thanks to
+# <http://samprocter.com/2014/06/documenting-a-language-using-a-custom-sphinx-domain-and-pygments-lexer/>.
+
+_cene_id = r'[^ \t\r\n`=;\',\.\\/()\[\]]*'
+
+class _CeneLexer(RegexLexer):
+    flags = re.DOTALL
+    tokens = {
+        'root': [
+            
+            # While there are some elaborate ways to write comments,
+            # \= line comments are by far the most common in practice.
+            # Highlighting the \;rm(...) comments as code is likely
+            # to be the desired behavior anyway. Unfortunately, \;uq=
+            # comments inside a string will be highlighted as code
+            # right now.
+            (r'\\=[^\r\n]*', Comment.Single),
+            
+            # An identifier preceded by ( or / is likely a macro name.
+            # Note that we don't highlight identifiers preceded by [
+            # even though it's equivalent to ( because in practice we
+            # use [ for delimiting strings.
+            (r'(?<=[(/])' + _cene_id, Name.Function),
+            
+            # An identifier preceded by ; is likely an escape
+            # sequence.
+            (r'(?<=[;])' + _cene_id, Keyword.Constant),
+            
+            # Anything else is text. We don't highlight identifiers in
+            # other contexts, because we don't know if they're part of
+            # a string. Even if we did know, some strings benefit from
+            # being highlighted as code.
+            (r'.', Text),
+        ],
+    }
 
 
 # General configuration for Sphinx
@@ -30,7 +72,12 @@ copyright = _copyright_dates + u' ' + author
 version = _package_json[u'version']
 release = version
 
+highlight_language = 'cene'
+
 todo_include_todos = True
+
+def setup(sphinx):
+    sphinx.add_lexer('cene', _CeneLexer())
 
 
 # Configuration for Sphinx HTML builds
